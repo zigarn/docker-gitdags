d966727bb317ab2700ec61cbb88083d2fc8a9366
Alexandre Garnier <zigarn@gmail.com>
2016-11-29 11:50:08 +0100

    Add verbose and debug modes

--- generateSVG.sh
+++ generateSVG.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
 BASE_PATH=$(cd $(dirname $0) && pwd -P)
+VERBOSE=false
+DEBUG=false
 
 generateSVG(){
     # FIXME: test file is .tex
     file=${1%.*}
-    pdflatex -output-directory=/tmp "${file}.tex" >/dev/null && pdf2svg "/tmp/$(basename ${file}).pdf" "${file}.svg"
+    $VERBOSE && echo "Generating SVG from $1"
+    $DEBUG && OUT=/dev/stdout || OUT=/dev/null
+    pdflatex -interaction=nonstopmode -output-directory=/tmp "${file}.tex" >$OUT && pdf2svg "/tmp/$(basename ${file}).pdf" "${file}.svg"
+    RESULT=$?
+    $VERBOSE && echo "Result: $([ $RESULT == 0 ] && echo OK || echo NOK)"
 }
 
+usage(){
+    echo "Usage: `basename $0` [-v] [-d] PATH..." >&2
+    echo "   -v   Verbose mode" >&2
+    echo "   -d   Debug mode (implies verbose)" >&2
+}
+
+while getopts vd opt; do
+    case "$opt" in
+        v) VERBOSE=true;;
+        d) VERBOSE=true; DEBUG=true;;
+        *) usage; exit 1;;
+    esac
+done
+shift $(($OPTIND-1))
 if [ "$#" -eq 0 ]; then
-    echo "Usage: `basename $0` PATH..." >&2
+    usage
     exit 1
 fi
 
